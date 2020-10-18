eb7a94d63ac2de0bb326b28ee23a85fbfe5b96a3
zoreander <ivo.capelo@gmail.com>
2020-10-18 22:39:14 +0100

    Implemented presets for camera

---
+++ Assets/Scripts/Camera/CameraRotationDefinition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct CameraRotationDefinition
+{
+    public KeyCode key;
+    public Vector3 rotationEuler;
+    public bool isAbsolute;
+}
