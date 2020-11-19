8976628e9be8e295daa1a49ffd3bd570789dec77
zoreander <ivo.capelo@gmail.com>
2020-11-19 18:29:45 +0000

    EndGameMenu

---
+++ Assets/Scripts/UI/MissionCompleteMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MissionCompleteMenu : MonoBehaviour
+{
+    [SerializeField]
+    private string exitScene;
+    
+    [SerializeField]
+    private string replayScene;
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(exitScene, LoadSceneMode.Single);
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(replayScene, LoadSceneMode.Single);
+    }
+}
